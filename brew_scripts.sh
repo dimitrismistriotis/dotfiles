bdce5ea95939ca46a9484face622d9b9d45a8051
Dimitrios Mistriotis <dimitrios.mistriotis@clearscore.com>
2018-02-20 17:32:28 +0000

    Brew location and fish shell

--- brew_scripts.sh
+++ brew_scripts.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
-
+#
 #  _                     _         _        _ _ 
 # | |__ _ _ _____ __ __ (_)_ _  __| |_ __ _| | |
 # | '_ \ '_/ -_) V  V / | | ' \(_-<  _/ _` | | |
 # |_.__/_| \___|\_/\_/  |_|_||_/__/\__\__,_|_|_|
-                                              
+#
+# Start here: https://brew.sh/
+#
+
 brew install diff-so-fancy
+brew install fish
