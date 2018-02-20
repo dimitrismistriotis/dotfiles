e5dc37360904b99da4d77735b12fb229454e098e
Dimitrios Mistriotis <dimitrios.mistriotis@clearscore.com>
2018-02-20 09:50:17 +0000

    So fancy; brew install script, etc

--- bin/aliases.sh
+++ bin/aliases.sh
@@ -1,4 +1,5 @@
 alias gai='git add -i'
+alias gca='git commit -a'
 
 # Rails
 alias reset_test_db='rake db:drop db:create db:schema:load RAILS_ENV=test'
@@ -7,7 +8,8 @@ alias reset_test_db='rake db:drop db:create db:schema:load RAILS_ENV=test'
 alias amend_commit='git commit --amend --no-edit'
 alias delete_merged_branches='git branch --merged | grep -v "\*" | grep -v "master" | xargs -n 1 git branch -d' # http://stackoverflow.com/questions/6127328/how-can-i-delete-all-git-branches-which-have-been-merged
 # Overwrite gd:
-alias gd='git diff --color | diff-highlight | strip_diff_leading_symbols' # Git diff with diff-highlight
+# alias gd='git diff --color | diff-highlight | strip_diff_leading_symbols' # Git diff with diff-highlight
+alias gd='git diff --color | diff-so-fancy'
 
 alias l='ls -G'
 
