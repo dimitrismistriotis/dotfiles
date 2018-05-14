479fb29e4ac496ef214952f8637aef013d4b35db
Your Name <dimitrios@mistriotis.com>
2018-05-14 09:06:05 +0100

    For git pull --rebase

--- bin/aliases.sh
+++ bin/aliases.sh
@@ -8,4 +8,4 @@ alias amend_commit='git commit --amend --no-edit'
 alias delete_merged_branches='git branch --merged | grep -v "\*" | grep -v "master" | xargs -n 1 git branch -d' # http://stackoverflow.com/questions/6127328/how-can-i-delete-all-git-branches-which-have-been-merged
 # Overwrite gd:
 alias gd='git diff --color | diff-highlight | strip_diff_leading_symbols' # Git diff with diff-highlight
-
+alias gpullr='git pull --rebase'
---
+++ fish/functions/gpullr.fish
@@ -0,0 +1,3 @@
+function gpullr
+    git pull --rebase
+end
