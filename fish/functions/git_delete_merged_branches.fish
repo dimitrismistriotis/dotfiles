a0e0e43dd34c84997d3b9ae8f7b4878ff3c6a9ab
Dimitrios Mistriotis <dimitrios@mistriotis.com>
2023-11-22 10:45:18 +0000

    Ported function from https://github.com/dimitrismistriotis/git-helpers/blob/master/functions/delete_merged_branches.fish

---
+++ fish/functions/git_delete_merged_branches.fish
@@ -0,0 +1,19 @@
+#
+# http://stackoverflow.com/questions/6127328/how-can-i-delete-all-git-branches-which-have-been-merged
+#
+function git_delete_merged_branches
+  #
+  # Existing merged branches
+  #
+  #                      All merged ones   | Not master, main, *
+  set merged_branches (git branch --merged | grep -v -E "master|main|\*")
+  # echo "merged_branches: $merged_branches"
+  if [ (count $merged_branches) -eq 0 ]
+    # echo "No branches to merge"
+    return 0
+  end
+
+
+	# Remove branches:
+	echo $merged_branches | xargs -n 1 git branch -d
+end
