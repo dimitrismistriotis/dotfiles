fac3f0eb6ffc935743ffc1fd88841cb9b355c79e
Dimitrios Mistriotis <dimitrios@mistriotis.com>
2025-09-04 09:57:05 +0100

    ask for value if not supplied

--- fish/functions/ytt_ollama.fish
+++ fish/functions/ytt_ollama.fish
@@ -2,8 +2,15 @@
 # ⏯️ As I use ytt | ollama to ask it to sumarise a YT Video
 #
 function ytt_ollama
-    echo $argv[1]
-    set transcript (ytt $argv[1])
+    if test -z "$argv[1]"
+        read -P "Enter YouTube URL: " yt_url
+    else
+        set yt_url $argv[1]
+    end
+
+    echo $yt_url
+
+    set transcript (ytt $yt_url)
     set llm_prompt "Please summarise the following text: <text> $transcript </text>. Do not prompt for any further actions."
     # echo $llm_prompt
     echo $llm_prompt | ollama run gemma3
