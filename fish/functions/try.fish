bf4b60a6b87bb4f4f356da6094b5c00dfcd936f4
Dimitrios Mistriotis <dimitrios@mistriotis.com>
2025-12-04 15:29:53 +0000

    hardcoded home to ~

--- fish/functions/try.fish
+++ fish/functions/try.fish
@@ -5,9 +5,9 @@ function try
     # Check if first argument is a known command
     switch $argv[1]
         case clone worktree init
-            set cmd (/usr/bin/env ruby $script_path --path "/home/dimitri0s/src/tries" $argv 2>/dev/tty)
+            set cmd (/usr/bin/env ruby $script_path --path "~/src/tries" $argv 2>/dev/tty)
         case '*'
-            set cmd (/usr/bin/env ruby $script_path cd --path "/home/dimitri0s/src/tries" $argv 2>/dev/tty)
+            set cmd (/usr/bin/env ruby $script_path cd --path "~/src/tries" $argv 2>/dev/tty)
     end
 
     set rc $status
